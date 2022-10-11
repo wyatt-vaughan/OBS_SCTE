bb5f52fd540f206a187b33226d36b3629060636f
Wyatt <69369798+WyattWantsToCode@users.noreply.github.com>
2022-10-10 21:56:44 -0500

    updated comments

--- server.py
+++ server.py
@@ -1,17 +1,26 @@
 import subprocess
 import os
-#subprocess.call([r'start.bat'])
+
 
 commandList = [
+    #tsp is the command for most TSduck commands
     "tsp",
+    #adds stuffing to the stream so the SCTE can be injected
     "--add-input-stuffing 1/10",
+    #is the input over srt that also needs to be what OBS streams to
     "-I srt 127.0.0.2:9999",
+    #adds the new service that follows SCTE-35 guidelines
     "-P pmt --service 0001 --add-programinfo-id 0x43554549 --add-pid 600/0x86",
+    #listens over udp port 1000 for a XML to splice into the stream
     "-P spliceinject --udp 1000 --service 0001",
+    #a monitor to see when SCTE is injected
     "-P splicemonitor",
+    #outputs a report after stream ends
     "-P analyze",
+    #removes extra stuffing at the end
     "-P filter --negate --pid 0x1FFF",
-    "-O file C:/Users/wvaug/Videos/output.ts"
+    #specifies where the output file should be stored
+    "-O file video_output.ts"
     
 ]
 def startServer():
